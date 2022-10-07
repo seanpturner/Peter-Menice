ae57e6608120b10a680aa031477bd2af498ea80f
Julie <97622838+juliegeers@users.noreply.github.com>
2022-10-07 12:38:19 -0400

    changed TrackingRepo wordage from JpaRepositoryImplementation to JpaRepository.

--- Peter-Menice/src/main/java/com/petermenice/Peter/Menice/repos/TrackingRepo.java
+++ Peter-Menice/src/main/java/com/petermenice/Peter/Menice/repos/TrackingRepo.java
@@ -1,7 +1,9 @@
 package com.petermenice.Peter.Menice.repos;
 
 import com.petermenice.Peter.Menice.entities.Tracking;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
 
-public interface TrackingRepo extends JpaRepositoryImplementation<Tracking, Long> {
+
+public interface TrackingRepo extends JpaRepository<Tracking, Long> {
 }
